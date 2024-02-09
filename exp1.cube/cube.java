977afbf986332d206addbfc1d628a841e7a8f060
tonysunny606 <147145222+tonysunny606@users.noreply.github.com>
2024-02-09 15:57:17 +0530

    COMPLETED

---
+++ exp1.cube/cube.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class cube
+{
+	public static void main(String[] arg)
+	{
+		System.out.println("Enter the limit:");
+			Scanner sc=new Scanner(System.in);
+			int num=sc.nextInt();
+			for(int i=1;i<=num;i++)
+			{
+			System.out.println(i+"  "+"cube is"+":"+i*i*i);
+		}
+			}
+			}
+
